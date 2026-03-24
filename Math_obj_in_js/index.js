f582d330cccb19965906d1abd8661d8a37614de3
Parvesshrajh <parvesshrajhsivakumar@gmail.com>
2026-03-24 16:12:36 +0530

    finished Math function in js

---
+++ Math_obj_in_js/index.js
@@ -0,0 +1,18 @@
+console.log(Math.PI);  // inbuilt value of PI
+console.log(Math.E);   // inbuilt value of exponential
+console.log(Math.round(2.13));   // round value 2.13 --> 2
+console.log(Math.floor(2.53));   // round down  2.53 --> 2
+console.log(Math.ceil(2.11));    // round up    2.11 --> 3
+console.log(Math.pow(2,3));      // power fn    2,3  --> 8
+console.log(Math.log(3));        // log value   1.0986122886681096
+
+// Inbuilt Math functions of trignometry
+console.log(Math.sin(45));       
+console.log(Math.tan(45));
+console.log(Math.cos(45));
+console.log(Math.abs(34.4));
+console.log(Math.sign(5));
+
+// Maximum and Minimum finding function in Math
+console.log(Math.max(1,2,3));
+console.log(Math.min(1,2,3));

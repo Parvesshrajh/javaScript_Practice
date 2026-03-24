3edb51e2e9a6d7409781b9ea0e0fd55d286a4d74
Parvesshrajh <parvesshrajhsivakumar@gmail.com>
2026-03-24 15:11:14 +0530

    finished js intro

---
+++ javaScript_intro/index.js
@@ -0,0 +1,23 @@
+// JavaScript (JS) is a high-level programming language used to make websites interactive and dynamic.
+
+console.log(`Hello`); // This a print statement in javascript.
+
+window.alert(`This is an alert!`); // This is an alert which pop-up in the webpage as a pop-up box to show users a alert message.
+
+// Single line comments in js
+
+/* Multi-line 
+comments in 
+js */
+
+document.getElementById("myH1").textContent = `Hello`;
+
+/*  document --> This refers the entire HTML document
+                 It was a object of the HTML web
+                 DOM (Document Object Model) 
+    
+    getElementById() --> This is a method select's HTML element by its ID(which was unique)
+
+    textContent -->      This is a property of the object element.
+
+    = `Hello` -->        we are been changing the element property to this content. */

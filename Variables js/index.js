6308172800696dd1f870733df98d4b5eaa7da52f
Parvesshrajh <parvesshrajhsivakumar@gmail.com>
2026-03-24 15:22:35 +0530

    finished Variables in js

---
+++ Variables js/index.js
@@ -0,0 +1,24 @@
+/* 1.Declaration
+   2.Assignment */
+
+let x; //Declaration
+x = 100; // Assignment 
+
+console.log(`You are having ${x} rupees in pocket`);
+// ${} --> allows us to enter the variables with the String text
+
+console.log(typeof x); // Shows us the type of variable that is integer
+
+/* let --> changeable
+   const --> unchangeable
+   var --> function scope */
+
+// Type	      Example
+// Number	  10
+// String	  "hello"
+// Boolean	  true
+// Undefined  let x;
+// Null	      null
+// Object	  {}
+// Array	  []
+// Function	  function(){}

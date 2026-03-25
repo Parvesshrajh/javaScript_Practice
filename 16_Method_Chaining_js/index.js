be69b47936a309724676408c499aac6e55bfafb7
Parvesshrajh <parvesshrajhsivakumar@gmail.com>
2026-03-25 16:00:22 +0530

    Finished Method Chaining in JS

---
+++ 16_Method_Chaining_js/index.js
@@ -0,0 +1,4 @@
+// Calling multiple methods one after another
+let username = "  pArveSshrajh  ";
+username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase(); // the methods was called one after another in a logical manner known as method chaining
+console.log(username);

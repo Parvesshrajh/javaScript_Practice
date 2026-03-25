d0104d6e31eb239df776cd3b4f5814d2b1e7dca5
Parvesshrajh <parvesshrajhsivakumar@gmail.com>
2026-03-25 16:25:04 +0530

    Finished While loop & Do While loop in JS

---
+++ 19_While_Loop_&_Do_while_loop/index.js
@@ -0,0 +1,9 @@
+let n = 5;
+while(n>0){
+    console.log(n);
+    n--;
+}
+
+do{
+    console.log("HI");
+}while(n==4) // It was false but do will be execute at least once because it is before the while function so it execute at least once.

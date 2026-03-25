7c21ff814833a64d7570bd4851dd3720cdbf375e
Parvesshrajh <parvesshrajhsivakumar@gmail.com>
2026-03-25 16:07:41 +0530

    Finished Logical Operator in JS

---
+++ 17_Logical_Operators/index.js
@@ -0,0 +1,11 @@
+let a = 10;
+let b = 10;
+if(a==10 && b==10){         // Checks if both are true only pass true or else false
+    console.log(true);
+}
+if(a==10 || b==20){         // Checks if one condition is true then it pass true or else both was false it passes false
+    console.log(true);
+} 
+if(a!=20){                  // Checks if condition is not equal to the element then it will pass true or else false
+    console.log(true);
+}

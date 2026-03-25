7c146ae7e3739cb7a46b872ff9ddc6060db46096
Parvesshrajh <parvesshrajhsivakumar@gmail.com>
2026-03-25 15:48:35 +0530

    Finished Slicing in JS

---
+++ 15_Slicing_js/index.js
@@ -0,0 +1,5 @@
+const fullname = "BroCode";
+let firstname = fullname.slice(0,3);  // slice upto 0 to 2 char index's
+let lastname = fullname.slice(3,7);   // slice upto 3 to 6 char index's
+console.log(firstname);
+console.log(lastname);

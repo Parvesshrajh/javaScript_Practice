10e6bf0b84f0dc307275106ff3488c58bef01724
Parvesshrajh <parvesshrajhsivakumar@gmail.com>
2026-03-26 14:03:21 +0530

    Finished Rest Parameter in JS

---
+++ 26_Rest_Parameter/index.js
@@ -0,0 +1,10 @@
+function openFridge(...foods){  // If we set spread operator as a parameter and pass arguements to it. It will pack all elements to an array.
+    console.log(foods)
+}
+
+const food1 = "pizza";
+const food2 = "hamburger";
+const food3 = "hotdog";
+const food4 = "sushi";
+
+openFridge(food1,food2,food3,food4);

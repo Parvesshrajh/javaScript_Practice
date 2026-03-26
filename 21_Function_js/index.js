cf16a86e96702e5b39cf26adbf8f6b7ced29640f
Parvesshrajh <parvesshrajhsivakumar@gmail.com>
2026-03-26 11:04:09 +0530

    Finished Functions in JS

---
+++ 21_Function_js/index.js
@@ -0,0 +1,12 @@
+//Function in javascript
+// functions are a block of code which was able to reuseable when it was called in the program
+function happyBirthday(){
+    console.log("Happy Birthday To You!");
+}
+happyBirthday();
+// return type in function returs some datatype to the place where the function was called.
+function add(x,y){  // parameters which receive's the arguements
+    let result = x +y;
+    return result;
+}
+console.log(add(5,4)); // arguements that passed to the function 
